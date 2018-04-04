cd51e31c2b4a23342c3c2fb3d6728ff190eb4ab3
Ios Developer <ios_developer@ios-developerdemacbook-pro.local>
2018-04-04 09:04:31 +0800

    部分修改

--- SnapshotDemo/main.m
+++ SnapshotDemo/main.m
@@ -3,7 +3,7 @@
 //  SnapshotDemo
 //
 //  Created by Ios_Developer on 2018/3/28.
-//  Copyright © 2018年 com.beiWei36du. All rights reserved.
+//  Copyright © 2018年 com.hai. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
